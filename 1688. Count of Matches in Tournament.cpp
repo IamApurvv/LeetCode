ef1fa9d2570326bf418d4703ebec9142571872f5
Apurv <apurv9801@gmail.com>
2023-05-01 08:56:24 +0530

    Leetcode

---
+++ 1688. Count of Matches in Tournament.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int numberOfMatches(int n)
+    {
+        int count{};
+        if (n == 1)
+            return 0;
+        while (n != 1)
+        {
+            if (n % 2 == 0)
+            {
+                count += (n / 2);
+                n /= 2;
+            }
+            else
+            {
+                count += ((n - 1) / 2);
+                n = (n + 1) / 2;
+            }
+        }
+        return count;
+    }
+};
